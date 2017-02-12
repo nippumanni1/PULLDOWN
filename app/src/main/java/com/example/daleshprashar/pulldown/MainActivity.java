8d8bb111a763c1323d7d9d74ff9680d4b538e8e6
nippumanni1 <nippumanni1@gmail.com>
2017-02-12 14:25:56 -0600

    god is no.111

---
+++ app/src/main/java/com/example/daleshprashar/pulldown/MainActivity.java
@@ -0,0 +1,17 @@
+package com.example.daleshprashar.pulldown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public  void pulldown()
+    {
+        System.out.println("god help me please..");
+    }
+}
